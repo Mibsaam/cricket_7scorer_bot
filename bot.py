24962c61bb96e5cc28916fbaecfa36c28382d078
Mibsaam <73050703+Mibsaam@users.noreply.github.com>
2026-08-27 03:58:51 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from flask import Flask
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-BOT_TOKEN = "8670400703:AAFx9ZbF8Hzv3SCU9TyN9Mh-LlOuKzV6p-k"
+BOT_TOKEN = "8812331993:AAF3pOsIWH5nlnFgxdkDYmozNLGli_CNyc0"
 ADMIN_ID = 874225351
 AUTHORIZED_SCORERS = {ADMIN_ID}
 
