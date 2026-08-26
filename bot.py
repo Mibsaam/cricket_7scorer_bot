deac52e8b19c40b3de881aa5e4e32b7817f2e256
Mibsaam <73050703+Mibsaam@users.noreply.github.com>
2026-08-27 02:54:50 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from flask import Flask
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-BOT_TOKEN = "8670400703:AAFx9ZbF8Hzv3SCU9TyN9Mh-LlOuKzV6p-k"
+BOT_TOKEN = "8812331993:AAHKPvObvrR6NhBXZIhE9dN-lRPTbye9bdQ"
 ADMIN_ID = 874225351
 
 # 24/7 RENDER KEEP-ALIVE SERVER (Never Sleep)
